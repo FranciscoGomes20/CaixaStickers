ccf49be7cb6ea0d69bcff15f733e50a43ed07e32
FranciscoGomes20 <henriquecgomes10@gmail.com>
2021-07-11 20:51:04 -0500

    Autenticação do usuário

--- stickers/models.py
+++ stickers/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class CaixaStickers(models.Model):
+class CaixaSticker(models.Model):
     data_venda = models.DateField(auto_now_add=False)
     quantidade_vendidos = models.IntegerField()
     valor_unidade = models.DecimalField(max_digits=10 ,default=0, decimal_places=2)
@@ -10,7 +10,7 @@ class CaixaStickers(models.Model):
     def __str__(self):
         return "{}".format(self.data_venda)
 
-class TotalStickers(models.Model):
+class TotalSticker(models.Model):
     stickers_total = models.IntegerField(default=0)
 
     def __str__(self):
--- stickers/urls.py
+++ stickers/urls.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import login
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.loginUser, name='login'),
+    path('logout', views.logoutUser, name='logout'),
     path('excluir_venda/<id_sticker>', views.excluir_venda, name='excluir_venda'),
 ]
